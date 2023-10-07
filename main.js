754a9503af162edb61cd75119dfa7bdd83d5eca1
Yarce22 <alejomira96@gmail.com>
2023-10-07 10:10:14 -0500

    Se corrige el error de invocacion del metodo y se completa la clase 5

--- main.js
+++ main.js
@@ -5,7 +5,25 @@ const natalia = {
         'Curso Definitivo de HTML y CSS',
         'Curso Practico de HTML y CSS',
     ],
-    apobarCurso(nuevoCurso) {
+    aprobarCurso(nuevoCurso) {
         this.cursosAprobados.push(nuevoCurso);
     },
 };
+
+function Student(name, age, cursosAprobados) {
+    this.name = name;
+    this.age = age;
+    this.cursosAprobados = cursosAprobados;
+}
+Student.prototype.aprobarCurso = function (nuevoCurso) {
+    this.cursosAprobados.push(nuevoCurso);
+}
+
+const juanita = new Student(
+    'Juanita Alejandra',
+    15,
+    [
+        'Curso de introduccion a los videojuegos',
+        'Curso de creacion de personajes'
+    ]
+);
